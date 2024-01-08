d7444155fa8be0b4b1cb5b72d935f3e8447e16d5
AbiAcorn <68abi2002@gmail.com>
2024-01-08 00:41:00 -0500

    Initial commit

---
+++ src/components/login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import loginImg from '../assets/tu.jpg'
+export default function Login() {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+            <div className="hidden sm:block">
+            <img className="w-full h-full object-cover" src={loginImg} alt="sand"/>
+            </div>
+
+            <div className='bg-gray-400 flex flex-col justify-center'>
+                <form className='max-w-[400px] w-full max-h-[400px] h-full mx-auto bg-gray-900 p-9 px-10 rounded-lg'>
+                    <h2 className='text-4xl text-white font:bold text-center'>SIGN IN</h2>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <label>
+                            Username
+                        </label>
+                        <input className=" rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline:none" type="txt"/>
+                    </div>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <label>
+                            password
+                        </label>
+                        <input className="p-2 rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline:none"  type="password"/>
+                    </div>
+                    <div className='flex justify-between text-gray-400 py-2'>
+            
+                           <p className='flex items-center'><input className="mr-2" type="checkbox" /> Rememberr me</p>
+                           <p> Forgot Password</p>
+                    </div>
+                    <button className='w-full my-5 py-2 bg-teal-500 shawdow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Sign In</button>
+                </form>
+            </div>
+        </div>
+    )
+}
